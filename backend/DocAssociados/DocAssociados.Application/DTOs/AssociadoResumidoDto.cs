3fcf72ea8d00c385cffa78b21b986577e70ac4a0
riverson98 <riversonvicente@gmail.com>
2025-04-15 16:59:11 -0300

    Adiciona associadoResumido

---
+++ backend/DocAssociados/DocAssociados.Application/DTOs/AssociadoResumidoDto.cs
@@ -0,0 +1,14 @@
+﻿using DocAssociados.Application.Interfaces;
+using DocAssociados.Service.Application.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace DocAssociados.Service.Application.DTOs;
+
+public class AssociadoResumidoDto
+{
+    public Guid? Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public Status Status { get; set; }
+    public Funcoes Funcao { get; set; }
+}
