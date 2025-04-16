1b44d9ed11af2e89cfd06f93efaa851771a95df0
riverson98 <riversonvicente@gmail.com>
2025-04-16 15:34:28 -0300

    #Frontend - corrige redirecionamento entre os componentes filhos

--- frontend/doc-brasil/src/app/modules/pages/home/home.component.ts
+++ frontend/doc-brasil/src/app/modules/pages/home/home.component.ts
@@ -1,18 +1,55 @@
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { DashboardStateService } from '../../../core/services/behaviorService/DashboardStateService.service';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { LoadingService } from '../../../core/services/loading/loading.service';
+import { AssociatesComponent } from './associates/associates.component';
+import { MembersComponent } from './members/members.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
+    RouterLink,
     RouterOutlet,
+    CommonModule,
+    ReactiveFormsModule 
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor(public loading: LoadingService) {}
+export class HomeComponent implements AfterViewInit{
+  query: string = '';
+  page:number = 1;
+  pageSize:number = 10;
+  filterControl = new FormControl('');
+
+  @ViewChild(AssociatesComponent) associates!: AssociatesComponent;
+  @ViewChild(MembersComponent) members!: MembersComponent;
+
+  constructor(public state: DashboardStateService, public loading: LoadingService, private router: Router, private cdr: ChangeDetectorRef) {}
+  
+  ngAfterViewInit(): void {
+    const currentRoute = this.router.url;
+
+    if (currentRoute.includes('/associados') && this.associates) {
+      this.onControlChange(this.associates, this.filterControl)
+      .subscribe((response) => console.log(response));
+    } else if (currentRoute.includes('/membros') && this.members) {
+      this.onControlChange(this.members, this.filterControl)
+      .subscribe((response) => console.log(response));
+    }
+  }
+
+  private onControlChange(component:any, control: FormControl): Observable<any> {
+    return control.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap(query => component.reloadData(this.page, this.pageSize, query))
+    )
+  }
 }
